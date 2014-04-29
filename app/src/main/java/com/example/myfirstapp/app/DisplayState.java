cc5954ca2558c07984e04f1c78c9eea97d122982
Kim Faughnan <faugh22k@mtholyoke.edu>
2014-04-29 17:39:43 -0400

    Added complete/incomplete Display states

--- app/src/main/java/com/example/myfirstapp/app/DisplayState.java
+++ app/src/main/java/com/example/myfirstapp/app/DisplayState.java
@@ -4,5 +4,5 @@ package com.example.myfirstapp.app;
  * Created by kcf412 on 4/29/14.
  */
 public enum DisplayState {
-    low, normal, high, current, future, category, all;
+    low, normal, high, current, future, complete, incomplete, category, all;
 }
